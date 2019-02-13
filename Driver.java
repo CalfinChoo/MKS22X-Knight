59203ffa0b28643387a6e2f0e80cbd04da895914
Calvin Chu <cchu00@stuy.edu>
2019-02-13 11:29:41 -0500

    fixed toString and added a Driver

---
+++ Driver.java
@@ -0,0 +1,12 @@
+public class Driver{
+  public static void main(String[] args) {
+    KnightBoard test = new KnightBoard(8, 8);
+    int num = 0;
+    for (int r = 0; r < test.board.length; r++) {
+      for (int c = 0; c < test.board[r].length; c++) {
+        test.board[r][c] = num++;
+      }
+    }
+    System.out.println(test.toString());
+  }
+}
--- KnightBoard.java
+++ KnightBoard.java
@@ -1,6 +1,6 @@
 public class KnightBoard{
   int[][] board;
-  @throws IllegalArgumentException when either parameter is negative.
+  //@throws IllegalArgumentException when either parameter is negative.
   public KnightBoard(int startingRows,int startingCols) {
       board = new int[startingRows][startingCols];
   }
@@ -8,21 +8,21 @@ public class KnightBoard{
     String s = "";
     for (int r = 0; r < board.length; r++) {
       for (int c = 0; c < board[r].length; c++) {
-        if (board[r][c] % 10 > 0) s += board[r][c];
-        else s += " " + board[r][c];
+        if (board[r][c] / 10 > 0) s += board[r][c] + " ";
+        else s += " " + board[r][c] + " ";
       }
       s += "\n";
     }
     return s;
   }
-  @throws IllegalStateException when the board contains non-zero values.
-  @throws IllegalArgumentException when either parameter is negative
-   or out of bounds.
-  public boolean solve(int startingRow, int startingCol) {}
-  @throws IllegalStateException when the board contains non-zero values.
-  @throws IllegalArgumentException when either parameter is negative
-   or out of bounds.
-  public int countSolutions(int startingRow, int startingCol) {}
-  private boolean solveH(int row ,int col, int level) {}
+  //@throws IllegalStateException when the board contains non-zero values.
+  //@throws IllegalArgumentException when either parameter is negative
+  // or out of bounds.
+  public boolean solve(int startingRow, int startingCol) {return true;}
+  //@throws IllegalStateException when the board contains non-zero values.
+  //@throws IllegalArgumentException when either parameter is negative
+  // or out of bounds.
+  public int countSolutions(int startingRow, int startingCol) {return 0;}
+  private boolean solveH(int row ,int col, int level) {return true;}
   // level is the # of the knight
 }
