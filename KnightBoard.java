be4a7d43257a84a07b4556ada9a500c415556045
Calvin Chu <cchu00@stuy.edu>
2019-02-13 11:48:49 -0500

    fixed toString again and started solve

--- KnightBoard.java
+++ KnightBoard.java
@@ -8,8 +8,11 @@ public class KnightBoard{
     String s = "";
     for (int r = 0; r < board.length; r++) {
       for (int c = 0; c < board[r].length; c++) {
-        if (board[r][c] / 10 > 0) s += board[r][c] + " ";
-        else s += " " + board[r][c] + " ";
+        if (board[r][c] == 0) s += " _ ";
+        else {
+          if (board[r][c] / 10 > 0) s += board[r][c] + " ";
+          else s += " " + board[r][c] + " ";
+        }
       }
       s += "\n";
     }
@@ -18,11 +21,22 @@ public class KnightBoard{
   //@throws IllegalStateException when the board contains non-zero values.
   //@throws IllegalArgumentException when either parameter is negative
   // or out of bounds.
-  public boolean solve(int startingRow, int startingCol) {return true;}
+  public boolean solve(int startingRow, int startingCol) {
+    return solveH(startingRow, startingCol, 1);
+  }
   //@throws IllegalStateException when the board contains non-zero values.
   //@throws IllegalArgumentException when either parameter is negative
   // or out of bounds.
   public int countSolutions(int startingRow, int startingCol) {return 0;}
-  private boolean solveH(int row ,int col, int level) {return true;}
+  private boolean solveH(int row ,int col, int level) {
+    //return true;
+    if (level = board.length * board[0].length) return true;
+    else {
+      if (row < board.length && row >= 0 && col < board[row].lengthboard && col >= 0 && [row][col] == 0) {
+        board[row][col] = level;
+        return
+      }
+    }
+  }
   // level is the # of the knight
 }
