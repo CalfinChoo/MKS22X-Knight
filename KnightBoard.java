baca8469acf7031c837f2a8728eb936f14dfe004
Calvin Chu <cchu00@stuy.edu>
2019-02-13 11:20:40 -0500

    added constructor, toString, gitignore

--- KnightBoard.java
+++ KnightBoard.java
@@ -1,13 +1,26 @@
 public class KnightBoard{
+  int[][] board;
   @throws IllegalArgumentException when either parameter is negative.
-  public KnightBoard(int startingRows,int startingCols) {}
-  public String toString() {}
+  public KnightBoard(int startingRows,int startingCols) {
+      board = new int[startingRows][startingCols];
+  }
+  public String toString() {
+    String s = "";
+    for (int r = 0; r < board.length; r++) {
+      for (int c = 0; c < board[r].length; c++) {
+        if (board[r][c] % 10 > 0) s += board[r][c];
+        else s += " " + board[r][c];
+      }
+      s += "\n";
+    }
+    return s;
+  }
   @throws IllegalStateException when the board contains non-zero values.
-  @throws IllegalArgumentException when either parameter is negative 
+  @throws IllegalArgumentException when either parameter is negative
    or out of bounds.
   public boolean solve(int startingRow, int startingCol) {}
-  @throws IllegalStateException when the board contains non-zero values. 
-  @throws IllegalArgumentException when either parameter is negative 
+  @throws IllegalStateException when the board contains non-zero values.
+  @throws IllegalArgumentException when either parameter is negative
    or out of bounds.
   public int countSolutions(int startingRow, int startingCol) {}
   private boolean solveH(int row ,int col, int level) {}
