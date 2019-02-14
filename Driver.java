5daca257ef2d4511dcbf01c085d1e28bc71c6349
Calvin Chu <cchu00@stuy.edu>
2019-02-14 11:55:27 -0500

    classwork

--- Driver.java
+++ Driver.java
@@ -1,12 +1,13 @@
 public class Driver{
   public static void main(String[] args) {
-    KnightBoard test = new KnightBoard(8, 8);
+    KnightBoard test = new KnightBoard(6, 6);
     // int num = 0;
     // for (int r = 0; r < test.board.length; r++) {
     //   for (int c = 0; c < test.board[r].length; c++) {
     //     test.board[r][c] = num++;
     //   }
     // }
+    test.solve(0,5);
     System.out.println(test.toString());
   }
 }
--- KnightBoard.java
+++ KnightBoard.java
@@ -30,13 +30,25 @@ public class KnightBoard{
   public int countSolutions(int startingRow, int startingCol) {return 0;}
   private boolean solveH(int row ,int col, int level) {
     //return true;
-    if (level = board.length * board[0].length) return true;
-    else {
-      if (row < board.length && row >= 0 && col < board[row].lengthboard && col >= 0 && [row][col] == 0) {
+    if (level == board.length * board[0].length) return true;
+    if (level < board.length * board[0].length) {
+      if (row < board.length && row >= 0 && col < board[row].length && col >= 0) {
+        if (board[row][col] == 0) {
         board[row][col] = level;
-        return
+        // for (int r = -2; r < 3; r++) {
+        //   for (int c = -2; c < 3; c++) {
+        //     if (r != 0 && c != 0 && solveH(row + r, col + c, level + 1)) {
+        //       return true;
+        //     }
+        //     board[row][col] = 0;
+        //   }
+        // }
+        // return solveH(row-1, col-2, level+1) || solveH(row-2, col-1, level+1) || solveH(row-2, col+1, level+1) || solveH(row-1, col+2, level+1) ||
+        //        solveH(row+1, col+2, level+1) || solveH(row+2, col+1, level+1) || solveH(row+2, col-1, level+1) || solveH(row+1, col-2, level+1);
+        }
       }
     }
+    return false;
   }
   // level is the # of the knight
 }
