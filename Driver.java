e5f52530a325040c5121a4e34bca14f9f0fb1f2e
Calvin Chu <cchu00@stuy.edu>
2019-02-25 11:45:24 -0500

    classwork

--- Driver.java
+++ Driver.java
@@ -1,13 +1,13 @@
 public class Driver{
   public static void main(String[] args) {
-    KnightBoard test = new KnightBoard(8, 8);
+    KnightBoard test = new KnightBoard(1, 1);
     // int num = 0;
     // for (int r = 0; r < test.board.length; r++) {
     //   for (int c = 0; c < test.board[r].length; c++) {
     //     test.board[r][c] = num++;
     //   }
     // }
-    test.solve(1,0);
+    //test.solve(0,0);
     System.out.println(test.toString());
   }
 }
