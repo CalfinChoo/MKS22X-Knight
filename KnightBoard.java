147d10ae097ce6b2b21784dc249369792449c573
Calvin Chu <cchu00@stuy.edu>
2019-02-26 23:16:56 -0500

    added addKnight and remKnight for a cleaner non optimized solve function

--- KnightBoard.java
+++ KnightBoard.java
@@ -1,24 +1,9 @@
 public class KnightBoard{
   int[][] board;
-  int[] moves = {1, 2, 2, 1, 2, -1, 1, -2, -1, -2, -2, -1, -2, 1, -1, 2};
-  int[][] numMoves;
+  int[][] moves = {{1, 2}, {2, 1}, {2, -1}, {1, -2}, {-1, -2}, {-2, -1}, {-2, 1}, {-1, 2}};
   //@throws IllegalArgumentException when either parameter is negative.
   public KnightBoard(int startingRows,int startingCols) {
       board = new int[startingRows][startingCols];
-      numMoves = new int[startingRows][startingCols];
-      for (int r = 0; r < numMoves.length; r++) {
-        for ( int c = 0; c < numMoves[r].length; c++) {
-          if ((r == 0 || r == numMoves.length - 1) && (c == 0 || c == numMoves[r].length - 1)) numMoves[r][c] = 2;
-          if (((r == 0 || r == numMoves.length - 1) && (c == 1 || c == numMoves[r].length - 2)) ||
-             ((c == 0 || c == numMoves[r].length - 1) && (r == 1 || r == numMoves.length - 2))) numMoves[r][c] = 3;
-          if (((r == 0 || r == numMoves.length - 1) && (c > 1 && c < numMoves[r].length - 2)) ||
-             ((c == 0 || c == numMoves[r].length - 1) && (r > 1 && r < numMoves.length - 2)) ||
-             ((r == 1 || r == numMoves.length - 2) && (c == 1 || c == numMoves[r].length - 2))) numMoves[r][c] = 4;
-          if (((r == 1 || r == numMoves.length - 2) && (c > 1 && c < numMoves[r].length - 2)) ||
-             ((c == 1 || c == numMoves[r].length - 2) && (r > 1 && r < numMoves.length - 2))) numMoves[r][c] = 6;
-          if ((r > 1 && r < numMoves.length - 2 && c > 1 && c < numMoves[r].length - 2)) numMoves[r][c] = 8;
-        }
-      }
   }
   public String toString() {
     String s = "";
@@ -54,29 +39,31 @@ public class KnightBoard{
   //@throws IllegalArgumentException when either parameter is negative
   // or out of bounds.
   public int countSolutions(int startingRow, int startingCol) {return 0;}
+  private boolean addKnight(int row, int col, int level) {
+    if (row < 0 || col < 0 || row >= board.length || col >= board[0].length) return false;
+    if (board[row][col] != 0) return false;
+    board[row][col] = level;
+    return true;
+  }
+  private boolean removeKnight(int row, int col){
+       if (row < 0 || col < 0 || row >= board.length || col >= board[0].length) return false;
+       if (board[row][col] == 0) return false;
+       board[row][col] = 0;
+       return true;
+   }
   private boolean solveH(int row ,int col, int level) {
-    System.out.println(toString());
-    if (level == board.length * board[0].length + 1) return true;
-    if (level < board.length * board[0].length + 1) {
-      if (board[row][col] == 0) {
-        board[row][col] = level;
-        for (int i = 0; i < moves.length; i+=2) {
-          if (row + moves[i] >= 0 && row + moves[i] < board.length && col + moves[i+1] >= 0 && col + moves[i+1] < board[row].length) {
-            if (solveH(row + moves[i], col + moves[i+1], level + 1)) {
-              return true;
-            }
-          }
+    //System.out.println(toString());
+    if (level > board.length * board[0].length) return true;
+    if (addKnight(row, col, level)) {
+      for (int i = 0; i < moves.length; i++) {
+        int r = row + moves[i][0];
+        int c = col + moves[i][1];
+        if (r >= 0 && r < board.length && c >= 0 && c < board[row].length && solveH(r, c, level + 1)) {
+          return true;
         }
-        board[row][col] = 0;
       }
-      }
+      removeKnight(row, col);
+    }
     return false;
   }
-  //&& board[row + moves[i]][col + moves[i+1]] == 0
-  // level is the # of the knight
-}
-class Point {
-  int possibleMoves;
-  int[] position = new int[2];
-  public Point()
 }
