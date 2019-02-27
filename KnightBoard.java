dff988e14508c083e700c4b03cfbd65f469cd2f8
Calvin Chu <cchu00@stuy.edu>
2019-02-27 01:47:05 -0500

    added comments to code, fin lab

--- KnightBoard.java
+++ KnightBoard.java
@@ -6,11 +6,11 @@ public class KnightBoard{
   int[][] moves = {{1, 2}, {2, 1}, {2, -1}, {1, -2}, {-1, -2}, {-2, -1}, {-2, 1}, {-1, 2}};
   //@throws IllegalArgumentException when either parameter is negative.
   public KnightBoard(int startingRows,int startingCols) {
-      board = new int[startingRows][startingCols];
-      optBoard = new int[startingRows][startingCols];
-      fillOptBoard();
+      board = new int[startingRows][startingCols];      //initializes board
+      optBoard = new int[startingRows][startingCols];   //initializes optimized board
+      fillOptBoard();                                   //fills in optimized board
   }
-  public void fillOptBoard() {
+  public void fillOptBoard() { //fills spaces with values depending on how many moves can be made from that space
     for (int r = 0; r < optBoard.length; r++) {
       for (int c = 0; c < optBoard[r].length; c++) {
         int count = 0;
@@ -52,14 +52,14 @@ public class KnightBoard{
     try{
       for (int[] r : board) {
         for (int c : r) {
-          if (c != 0) throw new IllegalStateException("Board is not empty");
+          if (c != 0) throw new IllegalStateException("Board is not empty");    //checks if board is empty; if not, throws exception
         }
       }
     } catch (IllegalStateException e) {
       e.printStackTrace();
       return false;
     }
-    return optSolveH(startingRow, startingCol, 1);
+    return optSolveH(startingRow, startingCol, 1);  //calls helper
   }
   //@throws IllegalStateException when the board contains non-zero values.
   //@throws IllegalArgumentException when either parameter is negative
@@ -68,94 +68,85 @@ public class KnightBoard{
     try{
       for (int[] r : board) {
         for (int c : r) {
-          if (c != 0) throw new IllegalStateException("Board is not empty");
+          if (c != 0) throw new IllegalStateException("Board is not empty");    //checks if board is empty; if not, throws exception
         }
       }
     } catch (IllegalStateException e) {
       e.printStackTrace();
     }
     board[startingRow][startingCol] = 1;
-    return countH(startingRow, startingCol, 2, 0);
+    return countH(startingRow, startingCol, 2, 0);  //calls helper
   }
   private int countH(int row, int col, int level, int sol) {
     if (level == board.length * board[0].length + 1) sol++;
     for (int i = 0; i < moves.length; i++) {
       int r = row + moves[i][0];
       int c = col + moves[i][1];
-      if (addKnight(r, c, level)) {
-        int solUpdate = countH(r, c, level + 1, sol);
+      if (addKnight(r, c, level)) {  //attempts to add knight
+        int solUpdate = countH(r, c, level + 1, sol);  //checks for a solution, updates the solution counter, continues
         if (solUpdate > sol) sol = solUpdate;
         removeKnight(r, c);
       }
     }
-    // if (addKnight(row, col, level)) {
-    //   for (int i = 0; i < moves.length; i++) {
-    //     int r = row + moves[i][0];
-    //     int c = col + moves[i][1];
-    //     int solUpdate = countH(r, c, level + 1, sol);
-    //     if (solUpdate > sol) sol = solUpdate;
-    //   }
-    //   removeKnight(row, col);
-    // }
-    return sol;
+    return sol;  //if no solutions are found, return num solutions
   }
-  private boolean addKnight(int row, int col, int level) {
+  private boolean addKnight(int row, int col, int level) {  // attempts to add a knight to a position
     if (row < 0 || col < 0 || row >= board.length || col >= board[0].length) return false;
     if (board[row][col] != 0) return false;
     board[row][col] = level;
     return true;
   }
-  private boolean removeKnight(int row, int col){
+  private boolean removeKnight(int row, int col){  // attempts to remove a knight from a position
        if (row < 0 || col < 0 || row >= board.length || col >= board[0].length) return false;
        if (board[row][col] == 0) return false;
        board[row][col] = 0;
        return true;
    }
-  private boolean solveH(int row ,int col, int level) {
+  private boolean solveH(int row ,int col, int level) {  //helper function of solveH (not optimized):
     //System.out.println(toString());
-    if (level > board.length * board[0].length) return true;
-    if (addKnight(row, col, level)) {
-      for (int i = 0; i < moves.length; i++) {
+    if (level > board.length * board[0].length) return true; //base case
+    if (addKnight(row, col, level)) { //attempts to add a knight to position
+      for (int i = 0; i < moves.length; i++) { //loops thru possible moves
         int r = row + moves[i][0];
         int c = col + moves[i][1];
-        if (r >= 0 && r < board.length && c >= 0 && c < board[row].length && solveH(r, c, level + 1)) {
+        if (r >= 0 && r < board.length && c >= 0 && c < board[row].length && solveH(r, c, level + 1)) { //returns true if solution is found
           return true;
         }
       }
-      removeKnight(row, col);
+      removeKnight(row, col); //removes knight if goes down a dead-end branch (backtracks)
     }
-    return false;
+    return false; //returns false if no solution is found
   }
-  private boolean optSolveH(int row, int col, int level) {
+  private boolean optSolveH(int row, int col, int level) { //helper function of solveH (optimized) :
     //System.out.println(toString());
     board[row][col] = level;
-    if (level == board.length * board[0].length) return true;
+    if (level == board.length * board[0].length) return true; //base case
     ArrayList<Coordinate> possMoves = new ArrayList<Coordinate>();
-    for (int i = 0; i < moves.length; i++) {
+    for (int i = 0; i < moves.length; i++) { //loops thru possible moves and adds them to a list
       int r = row + moves[i][0];
       int c = col + moves[i][1];
       if (r >= 0 && r < board.length && c >= 0 && c < board[r].length && board[r][c] == 0) {
         possMoves.add(new Coordinate(r, c, optBoard[r][c]));
       }
     }
-    Collections.sort(possMoves);
-    for (Coordinate i : possMoves) {
+    Collections.sort(possMoves);  //sorts list based on Coordinate class compareTo
+    for (Coordinate i : possMoves) {  //decrements from value in account for the knight's current position
       optBoard[i.getR()][i.getC()]--;
     }
-    for (Coordinate i : possMoves) {
+    for (Coordinate i : possMoves) {  //stores value before changing it and calling recursive function; if it backtracks the value can be retrieved
       int prev = optBoard[i.getR()][i.getC()]--;
       optBoard[i.getR()][i.getC()] = 0;
-      if (optSolveH(i.getR(), i.getC(), level + 1)) {
+      if (optSolveH(i.getR(), i.getC(), level + 1)) {  //returns true if solution is found
         return true;
       }
       optBoard[i.getR()][i.getC()] = prev;
     }
-    board[row][col] = 0;
+    board[row][col] = 0;  //returns false if no solution is found
     return false;
   }
 }
-class Coordinate implements Comparable<Coordinate> {
-  int value;
+class Coordinate implements Comparable<Coordinate> { //subclass for a space on the board containing the value
+  int value;  //value = number of possible moves from this space
   int r;
   int c;
   public Coordinate(int row, int col, int val) {
